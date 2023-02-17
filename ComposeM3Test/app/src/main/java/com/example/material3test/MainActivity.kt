d8a6e35ea3dc86cff1143d65d05d26e6f0aeff3a
Carter Roeser <carter@cdgtech.one>
2023-02-16 20:13:41 -0800

    Fix back button, cleanup

--- ComposeM3Test/app/src/main/java/com/example/material3test/MainActivity.kt
+++ ComposeM3Test/app/src/main/java/com/example/material3test/MainActivity.kt
@@ -31,15 +31,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material3TestTheme {
-                // A surface container using the 'background' color from the theme
-
                 Scaffold(
                     floatingActionButton = {
                         FloatingActionButton(onClick = {
                             val intent = intent
                             finish()
                             startActivity(intent)
-
                         }) {
                             Text("Reset")
                         }
@@ -56,26 +53,22 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun TextInputThing() {
         var text by remember { mutableStateOf("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \n\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") }
         var completionText by remember { mutableStateOf(" This is a test completion text. You can swipe to the right, or continue typing out this messsage to auto complete. If you hit the backspace, or enter a different character than from this message, the completion will be cleared.\n\nThis demo prevents you from moving the cursor into the suggested text section. If you tap on the suggested text, the cursor will move to the last character before the suggested text.\n\nAt the end of the suggested text, a \"Swipe\" icon is shown that goes away once the text is completed.") }
         var text1 by remember { mutableStateOf(TextFieldValue(text + completionText, TextRange(text.length, text.length))) }
-        // Change the following TextField to use TextFieldValue instead
+
         TextField(
             value = text1,
             onValueChange = {
-                Log.d("MainActivity", "onValueChange: change detected")
                 val previousText = text.plus(completionText)
                 if (it.text.length < previousText.length) {
-                    Log.d("MainActivity", "onValueChange: text length decreased")
                     text = it.text.substring(0, it.text.length - completionText.length)
                     completionText = ""
-                    text1 = TextFieldValue(text + completionText, TextRange(text.length, text.length))
+                    text1 = TextFieldValue(text + completionText, TextRange(it.selection.start, it.selection.end))
                 }
                 else if (it.text.length > previousText.length) {
-                    Log.d("MainActivity", "onValueChange: text length increased")
                     val newText = it.text.substring(0, it.text.length - completionText.length)
                     if (previousText.startsWith(newText)) {
                         // If there is a match, find the difference and move it from completionText to text
@@ -102,51 +95,33 @@ class MainActivity : ComponentActivity() {
                         completionText = ""
                         text1 = TextFieldValue(text + completionText, TextRange(text.length, text.length))
                     } else {
-                        // update cursor position
-                        Log.d("MainActivity", "selection: ${it.selection}")
-                        // Get first value from it.selection
-                        val selection = it.selection
-                        val selectionStart = selection.start
-                        val selectionEnd = selection.end
-
                         // if selectionEnd is less than index of completionText, then set selectionEnd to index of completionText
-                        if (selectionEnd < text.length) {
-                            text1 = TextFieldValue(
-                                text + completionText,
-                                TextRange(selectionStart, selectionEnd)
-                            )
-                        } else {
-                            if (selectionStart < text.length) {
-                                text1 = TextFieldValue(
-                                    text + completionText,
-                                    TextRange(selectionStart, text.length)
-                                )
-                            } else {
-                                text1 = TextFieldValue(
-                                    text + completionText,
-                                    TextRange(text.length, text.length)
-                                )
+                        var selectionStart = it.selection.start
+                        var selectionEnd = it.selection.end
+                        if (it.selection.end > text.length) {
+                            selectionEnd = text.length
+                            if (it.selection.start > text.length) {
+                                selectionStart = text.length
                             }
                         }
+                        text1 = TextFieldValue(text + completionText, TextRange(selectionStart, selectionEnd))
                     }
-
                 }
             },
             label = { Text("Autocompleting Demo Title") },
             visualTransformation = ColorsTransformation(completionText),
             modifier = Modifier
-                .pointerInput(Unit) {
-                    detectDragGestures { change, dragAmount ->
-                        Log.d("MainActivity", "dragAmount: $dragAmount")
-                        change.consumeAllChanges()
-                        // If drag is positive toward the right, then add completionText to text
-                        if (dragAmount.x > 0 && completionText != "") {
-                            text = text.plus(completionText)
-                            completionText = ""
-                            text1 = TextFieldValue(text + completionText, TextRange(text.length, text.length))
-                        }
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consumeAllChanges()
+                    // If drag is positive toward the right, then add completionText to text
+                    if (dragAmount.x > 0 && completionText != "") {
+                        text = text.plus(completionText)
+                        completionText = ""
+                        text1 = TextFieldValue(text + completionText, TextRange(text.length, text.length))
                     }
-                },
+                }
+            },
         )
     }
 }
@@ -162,7 +137,6 @@ class ColorsTransformation(private var completionText: String) : VisualTransform
 }
 
 fun buildAnnotatedStringWithColors(text:String, completionText: String): AnnotatedString{
-
     val builder = AnnotatedString.Builder()
     // Add text string before completion text string to builder
     val textBeforeCompletion = text.substring(0, text.length - completionText.length)
